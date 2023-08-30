9c423e89a8e106171a6129453c4a89323bd3abdf
Liam Townsley <contact@liamtownsley.me>
2023-08-30 23:52:03 +0100

    minor fix

--- src/commands/tickets/close.ts
+++ src/commands/tickets/close.ts
@@ -20,15 +20,15 @@ export default command(meta, {
     if (ticket) {
         const confirm = await askButtonQuestion('Are you sure you want to close this ticket?', interaction.channel as TextChannel, interaction.user.id, ['✅', '❌'])
         if (confirm == 1) {
-            await (interaction.channel as TextChannel).delete(`Ticked closed by: ${interaction.user.username} (${interaction.user.id})`).catch(() => {});
+            await (interaction.channel as TextChannel).delete(`Ticked closed by: ${interaction.user.username} (${interaction.user.id})`).catch(() => { });
         }
-        
-        if(ticket.message_id) {
+
+        if (ticket.message_id) {
             const freelancerChannel = await client.channels.fetch(keys.PENDING_ORDERS_CHANNEL) as TextChannel;
-            await freelancerChannel.messages.delete(ticket.message_id).catch(() => {});
-        }        
-    } else {
-        await interaction.channel.delete().catch(() => {});
+            await freelancerChannel.messages.delete(ticket.message_id).catch(() => { });
+        }
+    } else if (isTicket(interaction.channel as TextChannel)) {
+        await interaction.channel.delete().catch(() => { });
     }
 
 })
